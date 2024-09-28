585d255aa983b19c87b5b533b509196d8b2c6386
unknown <forkan.saadaoui@gmail.com>
2024-09-28 10:53:26 +0100

    custum validation with zod

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,6 +1,25 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const createArticleSchema = z.object({
-    title: z.string().min(2).max(200),
-    description: z.string().min(10),
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title should be a string",
+    })
+    .min(2, {
+      message:
+        "The title must contain at least 2 characters and a maximum of 200 characters.",
+    })
+    .max(200, {
+      message:
+        "The title must contain at least 2 characters and a maximum of 200 characters.",
+    }),
+  description: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title should be a string",
+    })
+    .min(10, {
+      message: "The description must contain at least 10 characters.",
+    }),
 });
