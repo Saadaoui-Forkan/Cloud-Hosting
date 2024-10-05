ed3cb2d27b388058da49f5202101cbb051e5f306
unknown <forkan.saadaoui@gmail.com>
2024-10-05 16:50:37 +0100

    protecting routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,20 @@ export function middleware(request: NextRequest) {
     const jwtToken = request.cookies.get('jwtToken')
     const token = jwtToken?.value as string
     if (!token ) { //To specify a particular route: !authToken && request.method === POST
-        return NextResponse.json(
-            { message: 'No Token Provided, Access Denied!' },
-            { status: 401 }
-        )
+        if (request.nextUrl.pathname.startsWith("/api/users/profile")) {
+            return NextResponse.json(
+                { message: 'No Token Provided, Access Denied!' },
+                { status: 401 }
+            )
+        }
+    } else {
+        if (request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/register") {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
     }
 }
 
 export const config = {
-    matcher: ['/api/users/profile/:path*'] //:path* ==> all routes after profile
+    matcher: ['/api/users/profile/:path*', '/login', '/register'] 
 }
+//:path* ==> all routes after profile
