ef1e6b4dfcb67020c0bfd845b786507d6e00cc28
unknown <forkan.saadaoui@gmail.com>
2024-10-09 17:06:06 +0100

    deployment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const ARTICLES_PER_PAGE = 6;
 
-export const DOMAIN = "https://cloud-hosting-eta.vercel.app/api"
+export const DOMAIN = "https://cloud-hosting-kappa.vercel.app/api"
 // export const DOMAIN = "http://localhost:3000/api"
